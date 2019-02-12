0ae49a5b717c94f8ed0209dd0f91b1602d7617eb
douglasbrandao21 <38235074+douglasbrandao21@users.noreply.github.com>
2019-02-12 00:14:35 -0200

    commit

--- with-conditional/App.js
+++ with-conditional/App.js
@@ -24,13 +24,30 @@
   }
   
   function Instruction(command, operators) {
-    this.command = command
+    this.command = command,
     this.operators = operators
+  }
+
+  function Label(name, adress) {
+    this.name = name
+    this.adress = adress
   }
 
   function fetchInstruction () {
       decodeInstruction()
-      const instructions = str.map( string => string.trim()).filter( string => !string.includes(':') )
+      let instructions = str.map( string => string.trim())
+      let labels = new Array
+      let j = 0
+
+      for (let i=0; i < instructions.length; i++) {
+        if(instructions[i].includes(':')) {
+          labels[j] = new Label(instructions[i].replace(':', ''), i)
+          j++
+        }   
+      }
+
+      instructions = instructions.filter( string => !string.includes(':') )
+
       let commands = new Array
       let aux = new Array
 
@@ -43,29 +60,57 @@
       }
 
       console.log(commands)
-
-      executeInstructions(instructions)
+      console.log(labels)
+      for(let i = 0; i < commands.length; i++) {
+        executeInstruction(instructions[i])
+      }
   }
 
-  function executeInstructions(instructions) {
-    
-    let haveJmp = 0
-    const jmps = ['jmp','je','jne','jg','jge','jl','jle']
-
-    for(i=0; i<instructions.length; i++) {
-      haveJmp = 0
-      for(j=0; j<jmps.length; j++) {
-        if(instructions[i].includes(jmps[j]))
-          haveJmp = 1
-      }
-      console.log(haveJmp)
-      if(haveJmp == 1) {
-        //Do stuff
-      } else {
-        //Do another stuff
-      }
-
+  function executeInstruction(instruction) {
+    switch(instruction.command) {
+      case 'jmp':
+        jmp(instruction, labels)
+        break
+      case 'je':
+        je(instruction, labels)
+        break
+      case 'jne':
+        jne(instruction, labels)
+        break
+      case 'jg':
+        jg(instruction, labels)
+        break
+      case 'jge':
+        jge(instruction, labels)
+        break
+      case 'jl':
+        jl(instruction, labels)
+        break
+      case 'jle':
+        jle(instruction, labels)
+        break
+      case 'movl':
+        movl(instruction)
+        break
+      case 'addl':
+        addl(instruction)
+        break
+      case 'incl':
+        incl(instruction)
+        break
+      case 'cmpl':
+        cmpl(instruction)
+        break
+      case 'leave':
+        leave()
+        break
+      case 'ret':
+        ret()
+        breal 
     }
+    
+    
+
   }
 
   
